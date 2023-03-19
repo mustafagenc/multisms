2216c2d8df8ab1750f8051983ca26fe1321d0911
Mustafa Genç <eposta@mustafagenc.info>
2023-03-19 15:50:21 +0300

    IletiMerkezi provider updated

--- src/Providers/MultiSms.IletiMerkezi.Provider/Configurations.cs
+++ src/Providers/MultiSms.IletiMerkezi.Provider/Configurations.cs
@@ -1,6 +1,32 @@
-﻿namespace Microsoft.Extensions.DependencyInjection;
+﻿using MultiSms.IletiMerkezi.Provider;
+using MultiSms.IletiMerkezi.Provider.Options;
+using MultiSms.Interfaces;
+
+namespace Microsoft.Extensions.DependencyInjection;
 
 public static class Configurations
 {
+    public static MultiSmsBuilder UseIletiMerkezi(this MultiSmsBuilder builder, string username, string password)
+    {
+        return builder.UseIletiMerkezi(username, password, null);
+    }
 
+    public static MultiSmsBuilder UseIletiMerkezi(this MultiSmsBuilder builder, string username, string password, string orginator)
+    {
+        return builder.UseIletiMerkezi(options => { options.Username = username; options.Password = password; options.Orginator = orginator; });
+    }
+
+    public static MultiSmsBuilder UseIletiMerkezi(this MultiSmsBuilder builder, Action<IletiMerkeziProviderOptions> config)
+    {
+        var configuration = new IletiMerkeziProviderOptions();
+        config(configuration);
+
+        configuration.Validate();
+
+        builder.ServiceCollection.AddSingleton((s) => configuration);
+        builder.ServiceCollection.AddHttpClient<ISmsProvider, IletiMerkeziProvider>();
+        builder.ServiceCollection.AddHttpClient<IIletiMerkeziProvider, IletiMerkeziProvider>();
+
+        return builder;
+    }
 }
--- src/Providers/MultiSms.IletiMerkezi.Provider/Options/CustomProviderData.cs
+++ src/Providers/MultiSms.IletiMerkezi.Provider/Options/CustomProviderData.cs
@@ -3,9 +3,7 @@ namespace MultiSms.IletiMerkezi.Provider.Options;
 
 public static class CustomProviderData
 {
-
     public const string Username = "";
     public const string Password = "";
     public const string Orginator = "";
-
 }
